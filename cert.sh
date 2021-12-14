45f3471fe40a5587b711b08ec8ae3cc153175f65
robin <dequanzh@usc.edu>
2021-12-14 22:48:29 +0000

    add acme.sh and certificate routine

---
+++ cert.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+init() {
+  curl https://get.acme.sh | sh -s email=dequanzhang1995@hotmail.com
+}
+
+acme.sh() {
+ "/root/.acme.sh/"acme.sh --home "/root/.acme.sh/" "$@"
+}
+
+cron() {
+  #renew certs
+  top=uxui.win
+  acme.sh --issue --standalone -d $top -d www.$top -d vpn.$top
+  mkdir -p $top
+  acme.sh --install-cert -d $top --cert-file $top/cert.pem --key-file $top/key.pem --fullchain-file $top/fullchain.pem
+  cat $top/fullchain.pem $top/key.pem > $top/$top.pem
+  #reload services
+  systemctl restart haproxy #load balancer
+  systemctl restart ocserv  #openconnect-server
+}
+
+"$@"
